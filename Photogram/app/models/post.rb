cee308d53ec86c65a90e0c376ad79a7a1c563f8c
cmc33 <cmcguigan33@gmail.com>
2016-07-01 14:02:41 -0400

    more comment model stuff

--- Photogram/app/models/post.rb
+++ Photogram/app/models/post.rb
@@ -1,7 +1,9 @@
 class Post < ActiveRecord::Base  
+  
   validates :user_id, presence: true  
   validates :image, presence: true
 
+  has_many :comments, dependent: :destroy
   has_attached_file :image, styles: { :medium => "640x" }
   validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
   belongs_to :user 
